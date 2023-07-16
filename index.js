e7df1cfba1726414c6f76f53dd86c4ca9a803303
jamar gillard <jaydevelopment101@gmail.com>
2023-07-15 21:56:48 -0400

    new

---
+++ index.js
@@ -0,0 +1,72 @@
+//template_l2e2ifc
+
+//service_wlrmqie
+
+//t1ZIk4Wa_gVAFuIAF 
+
+//make fields required again dont forget on email
+let isModalOpen = false ;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+  }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+    }
+    else {
+      document.body.classList.remove("dark-theme")
+    }
+  }
+
+
+
+
+
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        'service_wlrmqie',
+        'template_l2e2ifc',
+        event.target,
+        "t1ZIk4Wa_gVAFuIAF"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on jamargillardfrontend@email.com"
+        );
+      });
+  }
+
+  function toggleModal(){
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open')
+    }
+    isModalOpen = true;
+document.body.classList += " modal--open";
+  }
