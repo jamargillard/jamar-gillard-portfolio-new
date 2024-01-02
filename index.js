10dc8a69cde7e7499b23edcc7f49b7c24f879b19
jamar gillard <jaydevelopment101@gmail.com>
2024-01-01 23:43:52 -0500

    ADDED resume, links, pfp change

--- index.js
+++ index.js
@@ -22,18 +22,7 @@ function moveBackground(event) {
   }
 }
 
-function moveBackground2(event) {
-  const shapes = document.querySelectorAll(".about-picture--container");
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
 
-  for (let i = 0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0;
-    const boolInt = isOdd ? -1 : 1;
-    // Added rotate after tutorial
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
-  }
-}
 function toggleContrast() {
     contrastToggle = !contrastToggle;
     if (contrastToggle) {
